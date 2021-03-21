a3ff86e41159915b5068e193a58c5fc52cadf8ba
Rebeca Canesin <rebecafguilherme@gmail.com>
2021-03-21 15:03:29 -0300

    rotas funcionando sendo chamadas diretamente

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,33 @@
 const { Router } = require('express')
 const ExampleRouter = require("./ExampleRouter")
-const UsersRouter = require("./UsersRouter")
-const ProductsRouter = require("./ProductsRouter")
-const OrdersRouter = require("./OrdersRouter")
+// const UsersRouter = require("./UsersRouter")
+// const ProductsRouter = require("./ProductsRouter")
+// const OrdersRouter = require("./OrdersRouter")
 const router = Router()
 
 // aqui vão todas as rotas
 router.use('/example', ExampleRouter);
 
-router.use('/users', UsersRouter);
-router.use('/users/:uid', UsersRouter);
-router.use('/users', UsersRouter);
-router.use('/users/:uid', UsersRouter);
-router.use('/users/:uid', UsersRouter);
+// router.use(UsersRouter);
+// router.use(ProductsRouter);
+// router.use(OrdersRouter)
 
-router.use('/products', ProductsRouter);
-router.use('/products/:productid', ProductsRouter);
-router.use('/products', ProductsRouter);
-router.use('/products/:productid', ProductsRouter);
-router.use('/products/:productid', ProductsRouter);
+// router.use('/users', UsersRouter);
+// router.use('/users/:uid', UsersRouter);
+// router.use('/users', UsersRouter);
+// router.use('/users/:uid', UsersRouter);
+// router.use('/users/:uid', UsersRouter);
 
-router.use('/orders', OrdersRouter);
-router.use('/orders/:orderId', OrdersRouter);
-router.use('/orders', OrdersRouter);
-router.use('/orders/:orderId', OrdersRouter);
-router.use('/orders/:orderId', OrdersRouter);
+// router.use('/products', ProductsRouter);
+// router.use('/products/:productid', ProductsRouter);
+// router.use('/products', ProductsRouter);
+// router.use('/products/:productid', ProductsRouter);
+// router.use('/products/:productid', ProductsRouter);
+
+// router.use('/orders', OrdersRouter);
+// router.use('/orders/:orderId', OrdersRouter);
+// router.use('/orders', OrdersRouter);
+// router.use('/orders/:orderId', OrdersRouter);
+// router.use('/orders/:orderId', OrdersRouter);
 
 module.exports = router
