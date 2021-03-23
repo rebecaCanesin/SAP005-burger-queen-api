048dca5b998ff57b3a9346983314bfe932a0277d
Rebeca Canesin <rebecafguilherme@gmail.com>
2021-03-23 19:47:15 -0300

    config update migrations created

--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const ControllerUsers = require('../controller/ControllerUsers')
+const UserController = require('../controller/ControllerUsers')
 
 const router = Router()
 
-router.get('/users', ControllerUsers.all);
+router.get('/users', UserController);
 // router.get('/users/:uid', ControllerUsers.GetUserIdRouter);
-router.post('/users', ControllerUsers.create);
+router.post('/users', UserController);
 // router.put('/users/:uid', ControllerUsers.PutUserIdRouter);
 // router.delete('/users/:uid', ControllerUsers.DeleteUserIdRouter);
 
