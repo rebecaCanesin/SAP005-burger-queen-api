2884f7c23ce57a2f867f46a80b66ae7db0443b19
Rebeca Canesin <rebecafguilherme@gmail.com>
2021-03-27 18:55:56 -0300

    model users updated

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,11 +1,9 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     static associate(models) {
-      Users.hasOne(models.Orders, {
+      Users.hasMany(models.Orders, {
         foreignKey:'user_id' 
       })
     }
